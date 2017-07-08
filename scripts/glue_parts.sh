b7a4fd57b01b50691faff2bbba60692641b32e0e
Damiano Venturin <dam@venturin.net>
2017-07-08 22:08:24 +0200

    now the user can control where the BAR is shown. Also improved keyboard configuration

--- scripts/glue_parts.sh
+++ scripts/glue_parts.sh
@@ -29,17 +29,17 @@ fi
 
 if [ ${SCREEN_LAYOUT} == "laptop_monitor" ]; then
 	# Disables VGA and Digital ports
-	xrandr --output VGA1 --off
-	xrandr --output DP1 --off
+	xrandr --output VGA-1 --off
+	xrandr --output DP-1 --off
 fi
 
 if [ ${SCREEN_LAYOUT} == "two_monitors_L_shaped" ]; then
 	# Turns off laptop monitor and turns on the 2 HP 24"
 	# one HP 24" is central 
 	# one HP 24" is pivoted on the right of the other HP
-	xrandr --output LVDS1 --off
-	xrandr --output DP1 --left-of VGA1
-	xrandr --output VGA1 --rotate left --right-of DP1
+	xrandr --output LVDS-1 --off
+	xrandr --output DP-1 --left-of VGA-1
+	xrandr --output VGA-1 --rotate left --right-of DP-1
 fi
 
 if [ ${SCREEN_LAYOUT} == "three_monitors_H_shaped" ]; then
@@ -53,5 +53,9 @@ cat ${parts_dir}/${SCREEN_LAYOUT} >> ${i3_dir}/config
 
 cat ${parts_dir}/floating_windows >> ${i3_dir}/config
 cat ${parts_dir}/scratchpad >> ${i3_dir}/config
+
+# replacing placeholder BAR_OUTPUT with ${BAR_OUTPUT} content
+cat ${parts_dir}/look.template | sed "s/BAR_OUTPUT/${BAR_OUTPUT}/g" > ${parts_dir}/look
 cat ${parts_dir}/look >> ${i3_dir}/config
+
 cat ${parts_dir}/apps_startup >> ${i3_dir}/config
--- scripts/kinesis.sh
+++ scripts/kinesis.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 # Binding for i3 wm:
-# This binds SuperR with Mod3 instead of Mod4.
-# In this way only SuperL is used as i3 modificator
+# Normally SuperR (keycode 134) and SuperL (keycode 133) are bond to the label "Mod4"
+# These commands:
+# * remove the bond of SuperR to the label "Mod4"
+# * bind SuperR to the label "Mod3".
+# In this way I can set only SuperL (Mod4) as chosen i3 modifier and 
+# by pressing SuperR i3 won't be triggered.
 xmodmap -e 'remove Mod4 = Super_R' &> /dev/null
 xmodmap -e 'add Mod3 = Super_R' &> /dev/null
+
+# This remaps CAPS (keycode 66) to ESC
+xmodmap -e 'keycode 66 = Escape NoSymbol Escape' 
 
 # This would remap Super_R as ISO_Level3_Shift"
 # The remapping works (visible in `xmodmap`) but dead keys are not triggered
 # http://superuser.com/questions/138708/xorg-how-can-i-map-altgr-to-the-capslock-key-to-toggle-3rd-level-symbols
 # xmodmap -e "keycode 134 = ISO_Level3_Shift"
 
-# Kinesis keyboard settings
+# This loads Kinesis's custom XKB layout and some additional parameters:
+# * caps:none,lv3 -> disables the CAPS button and leaves just Shift for maiusc
+# * rwin_switch,grp -> SuperR will load diacritics (if present in layout)
+# * alt_space_toggle -> toggles among specified variants (aka different languages)
 setxkbmap -model kinesis -layout us,us -variant kinesis_adv_dvorak_it,rus -option "caps:none,lv3:rwin_switch,grp:alt_space_toggle"
 
-# Repeat rate for keystrokes
+# Sets the repeat-rate for keystrokes
 xset r rate 700 80
+
+
 
